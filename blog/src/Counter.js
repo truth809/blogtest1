389003744c9986ed8f9b33d94f3ecbb3f454da88
DESKTOP-SUA2INR\hmj <truth809@naver.com>
2021-03-04 12:42:25 +0900

    callback

---
+++ blog/src/Counter.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+function Counter() {
+    const [number, setNumber] = useState(0)
+
+    const onIncrease = () => {
+        setNumber(aaa => aaa + 1)
+    }
+    const onDecrease = () => {
+        setNumber(aaa => aaa - 1)
+    }
+    
+    return (
+        <div>
+            <h1>{number}</h1>
+            <button onClick={onIncrease}>+1</button>
+            <button onClick={onDecrease}>-1</button>
+        </div>
+    )
+}
+
+export default Counter
---
+++ blog/src/Hello.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Hello({ color, name, isSpecial }) {
+  return (
+    <div style={{ color }}>
+      { isSpecial ? <b>*</b> : null }
+      안녕하세요 {name}
+    </div>
+  );
+}
+
+Hello.defaulProps = {
+  name: '이름없음'
+}
+
+export default Hello;
